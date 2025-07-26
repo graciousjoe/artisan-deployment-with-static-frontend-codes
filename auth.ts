47618fc8a2a442fa4efc1eaf467be217f0e85201
unknown <theoladejitoluwa@gmail.com>
2025-07-26 16:36:28 +0100

    google auth error

--- auth.ts
+++ auth.ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth"
+import Google from "next-auth/providers/google"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [],
+  providers: [Google],
 })
+
+// Valid callback URL http://localhost:3000/api/auth/callback/google
