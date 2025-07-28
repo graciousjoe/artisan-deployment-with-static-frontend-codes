8b42b92390bada9110e27ea15fd73b431d2fdbe5
unknown <theoladejitoluwa@gmail.com>
2025-07-28 14:20:04 +0100

    google log in

--- app/profile/page.js
+++ app/profile/page.js
@@ -1,7 +1,25 @@
-"use client";
+import { auth } from "@/auth";
+import { signInWithGoogle, signOutWithGoogle } from "./actions";
 
-import { signIn } from "next-auth/react";
+export default async function SignIn() {
+  const session = await auth();
+  console.log(session);
 
-export default function SignIn() {
-  return <button onClick={() => signIn("google")}>Sign in with Google</button>;
+  return (
+    <>
+      {!session ? (
+        <form action={signInWithGoogle}>
+          <button className="bg-blue-500 text-white rounded p-2" type="submit">
+            Sign in with Google
+          </button>
+        </form>
+      ) : (
+        <form action={signOutWithGoogle}>
+          <button className="bg-red-500 text-white rounded p-2" type="submit">
+            Sign out
+          </button>
+        </form>
+      )}
+    </>
+  );
 }
