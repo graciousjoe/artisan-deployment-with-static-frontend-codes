93abe61f0f614a41d24dcac9793021c9d426f8a5
unknown <theoladejitoluwa@gmail.com>
2025-08-01 06:44:08 +0100

    Happy new month

--- app/components/HeroSection.js
+++ app/components/HeroSection.js
@@ -2,15 +2,28 @@ import Image from "next/image";
 
 export default function HeroSection() {
   return (
-    <div className="container mx-auto h-[80vh] bg-[url('/hero-img.png')] bg-no-repeat bg-cover bg-center p-3 flex flex-col justify-end text-white items-center relative ">
-      <div className="relative z-10 flex flex-col items-center">
-        <h3 className="text-3xl font-bold pb-3">
-          Discover Skilled Local Artisns You Trust
-        </h3>
-        <p>
-          Every artisans on this platform are thoroughly vetted for quality
-          reliability and professionalism
-        </p>
+    <div className="relative h-[80vh] w-full bg-[url('/hero-img.png')] bg-no-repeat bg-cover bg-center">
+      {/* Overlay */}
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+
+      <div className="relative z-10 flex h-full flex-col items-center justify-end md:justify-center md:items-start">
+        <div className="w-full max-w-[900px] px-4 mx-auto text-white pb-10 md:pb-0 text-center md:text-left">
+          <h3 className="pb-3 text-3xl font-bold md:text-5xl lg:text-6xl">
+            Discover Skilled
+            <span className="hidden md:inline">
+              <br />
+            </span>{" "}
+            Local Artisans You
+            <span className="hidden md:inline">
+              <br />
+            </span>{" "}
+            Trust
+          </h3>
+          <p>
+            Every artisan on this platform is thoroughly vetted for quality,
+            reliability, and professionalism.
+          </p>
+        </div>
       </div>
     </div>
   );
