5c2c3923d8ddee8c7392380c4e3e3d560203b36f
unknown <theoladejitoluwa@gmail.com>
2025-08-02 17:48:46 +0100

    fix build error attempt 1

--- app/components/ChoseArtisans.js
+++ app/components/ChoseArtisans.js
@@ -1,10 +1,12 @@
 import React from "react";
-
+import Image from "next/image";
 export default function ChoseArtisans(props) {
   const { name, description, image } = props.artisan;
   return (
     <div className="flex flex-col p-4 border rounded-lg shadow-md ">
-      <img
+      <Image
+        width={400}
+        height={300}
         className="rounded-lg w-full h-[220px] md:h-[200px] lg:h-[350px] object-cover"
         src={image.src}
         alt={image.alt}
