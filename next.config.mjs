5e5a1a68bdf3656e8eb507896e71ab263abc99f7
graciousjoe <graciousjoe234@gmail.com>
2025-08-22 20:25:39 +0100

    updated frontend

--- next.config.mjs
+++ next.config.mjs
@@ -4,26 +4,13 @@
 // export default nextConfig;
 
 
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',  // Next.js 13+
-// }
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-
-  // ✅ Only export frontend pages (ignore API routes)
-  exportPathMap: async function () {
-    return {
-      '/': { page: '/' },   // homepage
-      // add more routes if you have them (e.g. '/about': { page: '/about' })
+    output: 'export',
+    trailingSlash: true,
+    images: {
+        unoptimized: true
     }
-  },
-}
+};
 
 export default nextConfig;
