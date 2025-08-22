139beb03b6cffd9d93f15c97090a4a91473a4cd3
graciousjoe <graciousjoe234@gmail.com>
2025-08-22 18:40:42 +0100

    updated frontend

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',  // Next.js 13+
-}
+const nextConfig = {};
 
 export default nextConfig;
+
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',  // Next.js 13+
+// }
+
+// export default nextConfig;
 
 
