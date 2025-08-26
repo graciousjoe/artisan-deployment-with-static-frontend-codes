86c35485c8d1e30f30df0bf6740fee3256db94d8
unknown <theoladejitoluwa@gmail.com>
2025-08-26 23:19:32 +0100

    second trial

--- auth.ts
+++ auth.ts
@@ -1,13 +1,13 @@
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+// import NextAuth from "next-auth";
+// import Google from "next-auth/providers/google";
 
-export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
-      clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
-    }),
-  ],
-});
+// export const { handlers, signIn, signOut, auth } = NextAuth({
+//   providers: [
+//     Google({
+//       clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+// });
 
 // Valid callback URL http://localhost:3000/api/auth/callback/google
