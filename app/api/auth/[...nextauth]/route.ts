2d8a8926c22860c61da42d81928b050a1767ce1e
unknown <theoladejitoluwa@gmail.com>
2025-08-26 23:01:54 +0100

    client side

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,5 @@
+"use client"
+
 import { handlers } from "@/auth"; // Referring to the auth.ts we just created
 
 export const { GET, POST } = handlers;
--- app/components/NewsLetter.js
+++ app/components/NewsLetter.js
@@ -1,7 +1,7 @@
 export default function NewsLetter() {
   return (
-    <div className="flex flex-col items-center justify-center gap-2 p-6 bg-gray-100 rounded-lg shadow-md">
-      <h3 className="text-lg md:text-xl font-semibold">SUBSCRIBE TO OUR NEWSLETTER</h3>
+    <div className="flex flex-col items-center justify-center gap-2 py-10 rounded-lg shadow-md">
+      <h3 className="text-lg md:text-xl mb-7 font-semibold">SUBSCRIBE TO OUR NEWSLETTER</h3>
       <p className="text-sm text-gray-600 max-w-md">
         Subscribe to our newsletter and get loads of interesting news and update
         on what we are up to sent to you regularly
@@ -12,7 +12,7 @@ export default function NewsLetter() {
           placeholder="Enter your email"
           className="px-3 py-1 border border-gray-300 rounded-md w-full"
         />
-        <button className=" bg-primary text-black px-3 py-1 rounded-md">
+        <button className=" bg-primary text-white px-3 py-1 rounded-md">
           Subscribe
         </button>
       </form>
